86180b4425d89b7ff5805148cdb5457b96696b70
GertiTheGod <nemeth.gergo.balazs@students.jedlik.eu>
2022-11-23 10:14:28 +0100

    3.

--- functions.py
+++ functions.py
@@ -12,5 +12,14 @@ def menu():
     print('4 - Meglévő irányító új adata')
     print('5 - Legtöbb yardot passzolt irányító')
     print('6 - Legkevesebb yardot passzolt irányító')
-    return input('Kérem válasszon az alábbi menüpontok közül')
+    return input('Kérem válasszon az alábbi menüpontok közül: ')
+
+def fajlBeolvasas():
+    file=open(filename,'r',encoding='utf-8')
+    file.readline()
+    for egysor in file:
+        darabolt=egysor.strip().split(';')
+        iranyitok.append(darabolt[0])
+        yardok.append(float(darabolt[1]))
+    file.close()
 
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from functions import *
+
+fajlBeolvasas()
+
+choice=''
+while choice!='0':
+    choice=menu()
+    if choice=='1':
+        pass
+    if choice=='2':
+        pass
+    if choice=='3':
+        pass
+    if choice=='4':
+        pass
+    if choice=='5':
+        pass
+    if choice=='6':
+        pass
